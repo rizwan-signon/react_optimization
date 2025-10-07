70e0d5ef928ae371fc1e7d3d82b87c417bfb5b6d
@rizwan.ullah <rizwan.ullah@plcgroup.com>
2025-10-07 10:12:33 +0500

    add how useMeme() works in react optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Child from "./components/Child";
 
 const App = () => {
-  return <div>App</div>;
+  console.log("App component rendered");
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <Child />
+      <p>count in app is {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click{" "}
+      </button>
+    </div>
+  );
 };
 
 export default App;
