07fb369472397915e851990c7f45dbfb04763895
rizwan ullah <rizwan.signon@gmail.com>
2025-10-12 22:09:09 +0500

    add useform custom hook to reuse logic

---
+++ src/hooks/useForm.js
@@ -0,0 +1,13 @@
+const useForm = (initailValues = {}) => {
+  const [values, setValues] = useState(initailValues);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const resetForm = () => {
+    setValues(initailValues);
+  };
+
+  return { values, handleChange, resetForm };
+};
