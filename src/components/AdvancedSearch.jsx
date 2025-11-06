a8e5021622366761e40dad4364ca318806fbd7fe
rizwan ullah <rizwan.signon@gmail.com>
2025-11-06 17:18:53 +0500

    render list of fruits

--- src/components/AdvancedSearch.jsx
+++ src/components/AdvancedSearch.jsx
@@ -3,19 +3,14 @@ import { Students } from "../utils/students";
 const AdvancedSearch = () => {
   const [data, setData] = useState(Students);
   const [query, setQuery] = useState("");
-  const fruits = [
-    "apple",
-    "banana",
-    "orange",
-    "mango",
-    "grape",
-    "pineapple",
-    "peach",
-    "kiwi",
-    "strawberry",
-    "blueberry",
-  ];
-  return <div></div>;
+  const fruits = ["apple", "banana", "orange", "apple", "kiwi"];
+  return (
+    <div>
+      {fruits.map((fruit, index) => (
+        <div key={index}>{fruit}</div>
+      ))}
+    </div>
+  );
 };
 
 export default AdvancedSearch;
